b7e46412cfcbb761ca93dcb7b3cd2bbae7173e19
DesJCodes <141986568+DesJCodes@users.noreply.github.com>
2024-07-31 22:40:29 -0400

    "Updated App.js to use React Navigation, added dependencies and updated package-lock.json"

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import screen1 from './components/screen1';
+import screen2 from './components/screen2';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+
+const Stack = createNativeStackNavigator();
+
+
+
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
+
+
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +34,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+
+
+export default App; 
