d0d0303287d255cb73f699b9134e44baaf557b7b
DesJCodes <141986568+DesJCodes@users.noreply.github.com>
2024-08-02 14:00:13 -0400

    "Added react-native-gifted-chat dependency and implemented GiftedChat component in Chat.js"

--- components/Chat.js.js
+++ components/Chat.js.js
@@ -1,28 +1,51 @@
-import { useEffect } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { useState, useEffect } from "react";
+import { StyleSheet, View } from 'react-native';
+import { GiftedChat } from "react-native-gifted-chat";
+
 
 const Chat = ({ route, navigation }) => {
     const { name } = route.params;
+    const [messages, setMessages] = useState([]);
+    const onSend = (newMessages) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+    }
 
     useEffect(() => {
-        navigation.setOptions
-            ({ title: name });
-    });
+        setMessages([
+            {
+                _id: 1,
+                text: "Hello developer",
+                createdAt: new Date(),
+                user: {
+                    _id: 2,
+                    name: "React Native",
+                    avatar: "https://placeimg.com/140/140/any",
+                },
+            },
+        ]);
+    }, []);
+
+
+    useEffect(() => {
+        navigation.setOptions({ title: name })
+    }, []);
 
     return (
-        <View style={styles.container}>
-            <Text>Welcome to the chat, {route.params.name}!</Text>
-        </View>
-    );
-};
+        <GiftedChat
+            messages={messages}
+            onSend={messages => onSend(messages)}
+            user={{
+                _id: 1
+            }}
+        />
+    )
+
+}
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+        flex: 1
+    }
 });
-
 
 export default Chat;
