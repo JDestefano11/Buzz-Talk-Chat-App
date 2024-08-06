158406ca172621585c2077f74da037b215065a86
DesJCodes <141986568+DesJCodes@users.noreply.github.com>
2024-08-06 18:10:35 -0400

    "Added NetInfo and AsyncStorage dependencies, and implemented network connectivity check in App.js"

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { useNetinfo } from "@react-native-community/netinfo";
+import { disableNetwork, enableNetwork } from "firebase/firestore";
 
 const Stack = createNativeStackNavigator();
 
@@ -20,12 +22,23 @@ const App = () => {
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
 
+  const netInfo = useNetinfo();
+
+  useEffect(() => {
+    if (netInfo.isConnected === false) {
+      disableNetwork(db);
+    } else {
+      enableNetwork(db);
+    }
+  }, [netInfo.isConnected]);
+
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Start'>
         <Stack.Screen name="Start" component={Start} />
         <Stack.Screen name="Chat">
-          {props => <Chat db={db} {...props} />}
+          {props => <Chat db={db} isConnected={netInfo.isConnected} {...props} />}
         </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
